635ec1ab036c11a56dde53980def6e5899274c9c
Monica Rivera Pun <mondev811@gmail.com>
2023-02-15 22:36:42 -0800

    Configure navigator styles

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -6,7 +6,14 @@ const Stack = createStackNavigator();
 
 export const Navigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: '#ffffff',
+        },
+      }}>
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Details" component={Details} />
     </Stack.Navigator>
