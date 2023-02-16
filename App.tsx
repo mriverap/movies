85d8718a9b448b7194cd6b00ea0088302d295e17
Monica Rivera Pun <mondev811@gmail.com>
2023-02-15 22:02:33 -0800

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Movies Application</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,7 @@
+**Movies Application**
+
+Resources:
+
+-[Movies](https://www.themoviedb.org/)
+
+-[Stack Navigator](https://reactnavigation.org/docs/stack-navigator/)
