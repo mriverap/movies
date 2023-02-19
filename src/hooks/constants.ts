adf4098bcf2c9f71c7d3e163731c311f46f0f43a
Monica Rivera Pun <mondev811@gmail.com>
2023-02-18 19:58:31 -0800

    Fix typos

--- src/hooks/constants.ts
+++ src/hooks/constants.ts
@@ -3,8 +3,8 @@ export const configuratioAPIUrl = 'https://api.themoviedb.org/3/configuration';
 export const imageBaseUrl = 'https://image.tmdb.org/t/p/';
 export const imageSize = ['w45', 'w92', 'w154', 'w185', 'w300', 'w500'];
 export const cardSize = [
-  {width: 45, heigth: 60},
-  {width: 92, heigth: 123},
+  {width: 45, height: 60},
+  {width: 92, height: 123},
   {width: 154, height: 205},
   {width: 185, height: 247},
   {width: 300, height: 400},
