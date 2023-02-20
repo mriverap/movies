19c5da0159079574480c28128179017807ecde39
Monica Rivera Pun <mondev811@gmail.com>
2023-02-19 19:20:25 -0800

    Add custom image size styles

--- src/hooks/constants.ts
+++ src/hooks/constants.ts
@@ -3,10 +3,10 @@ export const configuratioAPIUrl = 'https://api.themoviedb.org/3/configuration';
 export const imageBaseUrl = 'https://image.tmdb.org/t/p/';
 export const imageSize = ['w45', 'w92', 'w154', 'w185', 'w300', 'w500'];
 export const cardSize = [
-  {width: 45, height: 60},
-  {width: 92, height: 123},
-  {width: 154, height: 205},
-  {width: 185, height: 247},
-  {width: 300, height: 400},
-  {width: 500, height: 667},
+  {width: 45, height: 69},
+  {width: 92, height: 141},
+  {width: 154, height: 237},
+  {width: 185, height: 285},
+  {width: 300, height: 462},
+  {width: 500, height: 769},
 ];
