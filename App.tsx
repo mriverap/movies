031a63ff41438ed1ee58076035ce4add729321ce
Monica Rivera Pun <mondev811@gmail.com>
2023-02-22 22:37:48 -0800

    Ignore ViewPropTypes error

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {LogBox} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigator} from './src/navigation/Navigator';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
+
+LogBox.ignoreLogs(['ViewPropTypes']);
 
 const App = () => {
   return (
