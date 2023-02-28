12a0fbbf88011d6155ed3aaa2f621259bd4de935
Monica Rivera Pun <mondev811@gmail.com>
2023-02-27 23:14:41 -0800

    Add getColors function

---
+++ src/helpers/getColors.ts
@@ -0,0 +1,25 @@
+import {imageBaseUrl, imageSize as imageSizeArray} from '../helpers/constants';
+import ImageColors from 'react-native-image-colors';
+import {Movie} from '../interfaces';
+
+export const getColors = async (movie: Movie) => {
+  const uri = `${imageBaseUrl}${imageSizeArray[1]}${movie?.poster_path}`;
+  try {
+    const result = await ImageColors.getColors(uri, {
+      cache: false,
+      key: 'unique_key',
+    });
+    switch (result.platform) {
+      case 'android':
+        return {primary: result.dominant, secondary: result.average};
+      case 'ios':
+        return {primary: result.primary, secondary: result.secondary};
+      default:
+        throw new Error('Unexpected platform key');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  return undefined;
+};
